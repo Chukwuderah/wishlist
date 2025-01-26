ce70a17c5b5ec68ad1317a8caa4044a897d67933
Chukwuderah <pleasantchukwu@gmail.com>
2025-01-26 15:23:25 +0100

    wishlist

--- src/wishList.jsx
+++ src/wishList.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import duffle from "../src/assets/duffle.png";
+import gammaxx from "../src/assets/gammaxx.png";
 import frame from "../src/assets/Frame 608.png";
+import jacket from "../src/assets/jacket.png";
 
 const WishlistItem = ({
   image = duffle,
   name = "Gucci duffel bag",
   price = 960,
-  originalPrice = 1160,
+  originalPrice,
+  showDiscount = false,
 }) => {
   return (
     <div className="items">
       <div className="disc">
-        <span>-35%</span>
+        {showDiscount && <span>-35%</span>}
         <svg
           width="34"
           height="34"
@@ -44,36 +47,37 @@ const WishlistItem = ({
           <path
             d="M8.75 20.25C9.16421 20.25 9.5 19.9142 9.5 19.5C9.5 19.0858 9.16421 18.75 8.75 18.75C8.33579 18.75 8 19.0858 8 19.5C8 19.9142 8.33579 20.25 8.75 20.25Z"
             stroke="white"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M19.25 20.25C19.6642 20.25 20 19.9142 20 19.5C20 19.0858 19.6642 18.75 19.25 18.75C18.8358 18.75 18.5 19.0858 18.5 19.5C18.5 19.9142 18.8358 20.25 19.25 20.25Z"
             stroke="white"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M2.75 3.75H5.75L8 16.5H20"
             stroke="white"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M8 12.5H19.6925C19.7792 12.5001 19.8633 12.4701 19.9304 12.4151C19.9975 12.3601 20.0434 12.2836 20.0605 12.1986L21.4105 5.44859C21.4214 5.39417 21.42 5.338 21.4066 5.28414C21.3931 5.23029 21.3679 5.18009 21.3327 5.13717C21.2975 5.09426 21.2532 5.05969 21.203 5.03597C21.1528 5.01225 21.098 4.99996 21.0425 5H6.5"
             stroke="white"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       </div>
       <strong>{name}</strong>
       <p className="price">
-        ${price} <span>${originalPrice}</span>
+        ${price}
+        {originalPrice && <span>${originalPrice}</span>}
       </p>
     </div>
   );
@@ -87,15 +91,18 @@ const WishList = () => {
         <button type="button">Move All To Blog</button>
       </div>
       <div className="flex-container">
-        <WishlistItem />
+        <WishlistItem showDiscount={true} originalPrice={1160} />
+        <WishlistItem
+          image={gammaxx}
+          name="RGB liquid CPU Cooler"
+          price={750}
+        />
         <WishlistItem
           image={frame}
-          name="Another Bag"
+          name="GP11 Shooter USB Gamepad"
           price={750}
-          originalPrice={1000}
         />
-        <WishlistItem />
-        <WishlistItem />
+        <WishlistItem image={jacket} name="Quilted Satin Jacket" price={750} />
       </div>
     </section>
   );
