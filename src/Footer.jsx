2a1c7d5d7e9ff50f0c9465aca715882f675fa0e4
Chukwuderah <pleasantchukwu@gmail.com>
2025-01-26 23:37:33 +0100

    boilerplate stuff

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h1>Exclusive</h1>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About</li>
+                <li>Sign Up</li>
+            </ul>
+        </div>
+    );}
