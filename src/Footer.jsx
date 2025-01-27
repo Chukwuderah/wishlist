397168f24cad44ce3f6d95052ca900efea4a0ec7
Chukwuderah <pleasantchukwu@gmail.com>
2025-01-27 17:21:34 +0100

    empty footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,13 +2,5 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <h1>Exclusive</h1>
-            <ul>
-                <li>Home</li>
-                <li>Contact</li>
-                <li>About</li>
-                <li>Sign Up</li>
-            </ul>
-        </div>
+        <div className="footer"></div>
     );}
