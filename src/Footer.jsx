b5a70316f74e89613a01a48896ab4fe07b9849d7
Chukwuderah <pleasantchukwu@gmail.com>
2025-01-27 17:50:44 +0100

    second list item

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FirstList from "./first-list";
+import SecondList from "./secondList";
 
 const Footer = () => {
   return (
     <footer className="flex-container">
       <FirstList />
+      <SecondList />
     </footer>
   );
 };
