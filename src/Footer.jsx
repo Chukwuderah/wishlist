0a42e57e78c808a3328dd04de9d404cfb1560c55
Chukwuderah <pleasantchukwu@gmail.com>
2025-01-27 17:32:57 +0100

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import FirstList from "./first-list";
 
 const Footer = () => {
     return (
-        <div className="footer"></div>
+        <footer className="flex-container">
+            <FirstList />
+        </footer>
     );}
