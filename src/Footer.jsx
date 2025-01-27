d49985f32154c3ae826875d2e26d090ddd33eea8
Chukwuderah <pleasantchukwu@gmail.com>
2025-01-27 17:56:18 +0100

    third list item

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FirstList from "./first-list";
 import SecondList from "./secondList";
+import ThirdList from "./thirdList";
 
 const Footer = () => {
   return (
     <footer className="flex-container">
       <FirstList />
       <SecondList />
+      <ThirdList />
     </footer>
   );
 };
